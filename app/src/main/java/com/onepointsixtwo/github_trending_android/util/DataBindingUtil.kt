929ef27f04b6a452a1e76ce76affb5d63212a4aa
John Kartupelis <john.kartupelis@experian.com>
2018-03-03 13:28:05 +0000

    Setup data binding
    For both Markdown and for loading an image via URL

--- app/src/main/java/com/onepointsixtwo/github_trending_android/util/DataBindingUtil.kt
+++ app/src/main/java/com/onepointsixtwo/github_trending_android/util/DataBindingUtil.kt
@@ -1,7 +1,13 @@
 package com.onepointsixtwo.github_trending_android.util
 
 import android.databinding.BindingAdapter
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import org.markdownj.MarkdownProcessor
 
 
 @BindingAdapter("visible")
@@ -11,4 +17,17 @@ fun viewVisibilityAdapter(view: View, visible: Boolean) {
         visibility = View.GONE
     }
     view.visibility = visibility
+}
+
+@BindingAdapter("markdown")
+fun textViewMarkdownTextAdapter(textView: TextView, markdownText: String) {
+    val processor = MarkdownProcessor()
+    val htmlText = processor.markdown(markdownText)
+    textView.movementMethod = LinkMovementMethod()
+    textView.setText(Html.fromHtml(htmlText))
+}
+
+@BindingAdapter("imageURL")
+fun imageViewImageFromURLAdapter(imageView: ImageView, url: String) {
+    Picasso.with(imageView.context).load(url).into(imageView)
 }
